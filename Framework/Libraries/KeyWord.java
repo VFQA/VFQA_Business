1ad69f175ffe35cf9f4c7f5bd91aa67edf98bceb
ImranH2 <ImranH2@E0344453.internal.vodafone.com>
2018-04-24 16:02:16 +0300

    Commit

---
+++ Framework/Libraries/KeyWord.java
@@ -0,0 +1,551 @@
+package Libraries;
+
+import utilities.*;
+
+public class KeyWord {
+
+	Keyword_CRM KC = new Keyword_CRM();
+	Keyword_API KA = new Keyword_API();
+	Keyword_SIPT KS = new Keyword_SIPT();
+	Keyword_ETHERNET KM = new Keyword_ETHERNET();
+	Keyword_LEASEDLINE KL = new Keyword_LEASEDLINE();
+	Keyword_IPLC KI = new Keyword_IPLC();
+	Keyword_Validations KV = new Keyword_Validations();
+	Keyword_Putty KP = new Keyword_Putty();
+	Keyword_Guided KG = new Keyword_Guided();
+	Keyword_FixedLine KF = new Keyword_FixedLine();
+	Keyword_DB KDB = new Keyword_DB();
+	Keyword_OSM KO = new Keyword_OSM();
+	Keyword_PEARL KPL = new Keyword_PEARL();
+	Keyword_DAPN KD = new Keyword_DAPN();
+	Keyword_PJM KJ = new Keyword_PJM();
+	
+
+	Dialers KDialers = new Dialers();
+	SetCapabilities KSetCapabilities = new SetCapabilities();
+	MobileRTBCheck KRTB = new MobileRTBCheck();
+	Charging KCharging = new Charging();
+	Notification KNotification = new Notification();
+	Handset KHandset = new Handset();
+	USSD KUSSD = new USSD();
+	Siebel KSiebel = new Siebel();
+
+	// ------------------Keyword CRM -------------------//
+	public String Siebel_Login() {
+		return KC.Siebel_Login();
+	}
+
+	public String Siebel_Logout() {
+		return KC.Siebel_Logout();
+	}
+
+	public String ContactCreation() {
+		return KC.ContactCreation();
+	}
+
+	public String AccountCreation() {
+		return KC.AccountCreation();
+	}
+
+	public String AddressCreation() {
+		return KC.AddressCreation();
+	}
+
+	public String BillingProfileCreation() {
+		return KC.BillingProfileCreation();
+	}
+
+	public String SalesOrder() {
+		return KC.SalesOrder();
+	}
+
+	public String PlanSelection() {
+		return KC.PlanSelection();
+	}
+
+	public String OrderSubmission() {
+		return KC.OrderSubmission();
+	}
+
+	public String Entp_AccountCreation() {
+		return KC.Entp_AccountCreation();
+	}
+
+	public String Entp_ContactCreation() {
+		return KC.Entp_ContactCreation();
+	}
+
+	public String Modify() {
+		return KC.Modify();
+	}
+
+	public String UpgradePromotion() {
+		return KC.UpgradePromotion();
+	}
+
+	public String UpgradePromotion_Acc360() {
+		return KC.UpgradePromotion_Acc360();
+	}
+
+	public String SIMSwap() {
+		return KC.SIMSwap();
+	}
+
+	public String Change_MSISDN() {
+		return KC.Change_MSISDN();
+	}
+
+	public String Consumer_Migration() {
+		return KC.Consumer_Migration();
+	}
+
+	public String Enterprise_Migration() {
+		return KC.Enterprise_Migration();
+	}
+
+	public String RealTimeBalance_Screen() {
+		return KC.RealTimeBalance_Screen();
+	}
+
+	public String ModifySmartLimit() {
+		return KC.ModifySmartLimit();
+	}
+
+	public String ModifySmartLimit_Acc360() {
+		return KC.ModifySmartLimit_Acc360();
+	}
+
+	public String BillPayment() {
+		return KC.BillPayment();
+	}
+
+	public String Disconnection() {
+		return KC.Disconnection();
+	}
+
+	public String TroubleTicket() {
+		return KC.TroubleTicket();
+	}
+
+	public String Discounts() {
+		return KC.Discounts();
+	}
+
+	public String TransferOfService() {
+		return KC.TransferOfService();
+	}
+
+	public String Suspension() {
+		return KC.Suspension();
+	}
+
+	public String Resume() {
+		return KC.Resume();
+	}
+
+	public String SiebleValidation() {
+		return KC.SiebleValidation();
+	}
+
+	public String TransferOfOwnership_C2C() {
+		return KC.TransferOfOwnership_C2C();
+	}
+
+	public String TransferOfOwnership_E2E() {
+		return KC.TransferOfOwnership_E2E();
+	}
+
+	public String TransferOfOwnership_C2E() {
+		return KC.TransferOfOwnership_C2E();
+	}
+
+	public String TransferOfOwnership_E2C() {
+		return KC.TransferOfOwnership_E2C();
+	}
+
+	public String ServicePoint() {
+		return KC.ServicePoint();
+	}
+
+	public String Account360() {
+		return KC.Account360();
+	}
+
+	public String OpenUI_AccesRrights_6Segments() {
+		return KC.OpenUI_AccesRrights_6Segments();
+	}
+
+	public String OrderPayments() {
+		return KC.OrderPayments();
+	}
+
+	public String Barring() {
+		return KC.Barring();
+	}
+
+	public String UnBarring() {
+		return KC.UnBarring();
+	}
+
+	public String DunningProcess() {
+		return KC.DunningProcess();
+	}
+
+	public String DunningAction() {
+		return KC.DunningAction();
+	}
+
+	public String CollectionExit() {
+		return KC.CollectionExit();
+	}
+
+	public String Change_PrimaryNumber() {
+		return KC.Change_PrimaryNumber();
+	}
+
+	public String LanguageChange() {
+		return KC.LanguageChange();
+	}
+	
+	public String CreditLimit() {
+		return KC.CreditLimit();
+	}
+	
+	public String MVCareBill() {
+		return KC.MVCareBill();
+	}
+	
+	public String BillSummary() {
+		return KC.BillSummary();
+	}
+	
+	public String HappyOffers() {
+		return KC.HappyOffers();
+	}
+	
+	public String DueAmount() {
+		return KC.DueAmount();
+	}
+	
+	public String CancelOrder() {
+		return KC.CancelOrder();
+	}
+	// ------------------Keyword CRM -------------------//
+
+	// ------------------Keyword Guided -------------------//
+
+	public String SIMSwap_Guided_Journey() {
+		return KG.SIMSwap_Guided_Journey();
+	}
+
+	public String ConsumerPostpaid_GuidedJourney() {
+		return KG.ConsumerPostpaid_GuidedJourney();
+	}
+
+	public String SIMSwap_Global_Search() {
+		return KG.SIMSwap_Global_Search();
+	}
+	// ------------------Keyword Guided -------------------//
+
+	// ---------------------Keyword API------------------------//
+
+	public String RTB_Login() {
+		return KA.RTB_Login();
+	}
+
+	public String RTB() {
+		return KA.RTB();
+	}
+
+	public String RTB_Check() {
+		return KA.RTB_Check();
+	}
+
+	public String RTB_Compare() {
+		return KA.RTB_Compare();
+	}
+
+	// ---------------------Keyword API------------------------//
+
+	// ---------------------Keyword Validation------------------------//
+
+	public String RTB_Validation() {
+		return KV.RTB_Validation();
+	}
+
+	// ---------------------Keyword Validation------------------------//
+
+	// ---------------------Keyword SIPT------------------------//
+
+	public String SIPT() {
+		return KS.SIPT();
+	}
+
+	// ---------------------Keyword SIPT------------------------//
+
+	// ---------------------Keyword ETHERNET------------------------//
+
+	public String ETHERNET() {
+		return KM.ETHERNET();
+	}
+
+	// ---------------------Keyword ETHERNET------------------------//
+
+	// ---------------------Keyword LEASEDLINE------------------------//
+
+	public String LEASEDLINE() {
+		return KL.LEASEDLINE();
+	}
+
+	// ---------------------Keyword LEASEDLINE------------------------//
+
+	// ---------------------Keyword LEASEDLINE------------------------//
+
+	public String IPLC() {
+		return KI.IPLC();
+	}
+
+	// ---------------------Keyword LEASEDLINE------------------------//
+	// ---------------------Keyword_DAPN------------------------//
+
+	public String PlanSelection_DAPN() {
+		return KD.PlanSelection_DAPN();
+	}
+	// ---------------------Keyword_DAPN------------------------//
+
+	// ---------------------Keyword Putty------------------------//
+
+	public String LoginSSH() {
+		return KP.LoginSSH();
+	}
+
+	public String LogoutSSH() {
+		return KP.LogoutSSH();
+	}
+
+	public String BillGeneration_AccountLevel() {
+		return KP.BillGeneration_AccountLevel();
+	}
+
+	public String Invoicegeneration() {
+		return KP.Invoicegeneration();
+	}
+
+	public String Collections() {
+		return KP.Collections();
+	}
+	
+	public String GetZipFile() {
+		return KP.GetZipFile();
+	}
+	// ---------------------Keyword Putty------------------------//
+
+	// ---------------------Keyword_FixedLine------------------------//
+
+	public String PlanSelection_FL() {
+		return KF.PlanSelection_FL();
+	}
+
+	public String PlanSelection_FL_ENT() {
+		return KF.PlanSelection_FL_ENT();
+	}
+
+	public String FL_ONT_CPE_Replacement() {
+		return KF.FL_ONT_CPE_Replacement();
+	}
+
+	public String OrderVerfication() {
+		return KF.OrderVerfication();
+	}
+
+	// ---------------------Keyword_FixedLine------------------------//
+	// ---------------------Keyword_OSM------------------------//
+
+	public String OSM_Login() {
+		return KO.OSM_Login();
+	}
+
+	public String OSM_SearchFL() {
+		return KO.OSM_SearchFL();
+	}
+	
+	public String OSM_Pearl_data() {
+		return KO.OSM_Pearl_data();
+	}
+
+
+	// ---------------------Keyword_OSM------------------------//
+
+	// ---------------------Keyword CRM/BRM DB------------------------//
+	public String DBConnection() {
+		return KDB.DBConnection();
+	}
+
+	public String BillPoID() {
+		return KDB.BillPoID();
+	}
+
+	public String DBDisconnection() {
+		return KDB.DBDisconnection();
+	}
+	// ---------------------Keyword CRM/BRM DB------------------------//
+
+	// ---------------------Keyword_PEARL------------------------//
+	public String PlanSelection_Pearl() {
+		return KPL.PlanSelection_Pearl();
+	}
+
+	public String PlanSelection_Data_Pearl() {
+		return KPL.PlanSelection_Data_Pearl();
+	}
+	// ---------------------Keyword_PEARL------------------------//
+	
+	// ---------------------PJM ------------------------//
+	public String PJM_Login() {
+		return KJ.PJM_Login();
+	}
+	public String PJM_Upload() {
+		return KJ.PJM_Upload();
+	}
+	// ---------------------PJM ------------------------//
+	
+	// ---------------------Keyword Mobile Usages Starts------------------------//
+		public String Dialer() {
+			return KDialers.Dialer();
+		}
+
+		public String CheckUnBarringCall() {
+			return KDialers.CheckUnBarringCall();
+		}
+
+		public String CheckBarringCall() {
+			return KDialers.CheckBarringCall();
+		}
+
+		public String smsSender() {
+			return KDialers.smsSender();
+		}
+
+		public String BalanceCheckDialer() {
+			return KDialers.BalanceCheckDialer();
+		}
+
+		public String RechargeDialer() {
+			return KDialers.RechargeDialer();
+		}
+		
+		public String BillEnquiryDialler() {
+			return KDialers.BillEnquiryDialler();
+		}
+
+		public String setMessengerCapabilities() {
+			return KSetCapabilities.setMessengerCapabilities();
+		}
+
+		public String SetCallCapabilities() {
+			return KSetCapabilities.setDialerCapabilities();
+		}
+		
+		public String setMCareCapabilities() {
+			return KSetCapabilities.setMCareCapabilities();
+		}
+
+		public String PrevCheckBalance() {
+			return KRTB.PrevCheckBalance();
+		}
+
+		public String PostCheckBalance() {
+			return KRTB.PostCheckBalance();
+		}
+
+		public String LocalCallCharging() {
+			return KCharging.LocalCallCharging();
+		}
+
+		public String LocalSMSCharging() {
+			return KCharging.LocalSMSCharging();
+		}
+
+		public String InternationalCallCharging() {
+			return KCharging.InternationalCallCharging();
+		}
+
+		public String InternationalSMSCharging() {
+			return KCharging.InternationalSMSCharging();
+		}
+
+		public String BalanceCheck() {
+			return KNotification.BalanceCheck();
+		}
+
+		public String FlexBalanceCheck() {
+			return KNotification.FlexBalanceCheck();
+		}
+
+		public String ProductInfoNotification() {
+			return KNotification.ProductInfoNotification();
+		}
+
+		public String ConfigureSMSC() {
+			return KHandset.ConfigureSMSC();
+		}
+
+		public String RestartMobile() {
+			return KHandset.RestartMobile();
+		}
+
+		public String VerifyRecharge() {
+			return KUSSD.VerifyRecharge();
+		}
+
+		public String USSDJourney() {
+			return KUSSD.USSDJourney();
+		}
+
+		public String InvokeUSSDMenu() {
+			return KUSSD.InvokeUSSDMenu();
+		}
+
+		public String USSDCleaner() {
+			return KUSSD.USSDCleaner();
+		}
+
+		public String PunchRechargePIN() {
+			return KUSSD.PunchRechargePIN();
+		}
+
+		public String VerifyProductActivationPrepaid() {
+			return KUSSD.VerifyProductActivationPrepaid();
+		}
+
+		public String SearchAsset() {
+			return KSiebel.SearchAsset();
+		}
+
+		public String CheckOrder() {
+			return KSiebel.CheckOrder();
+		}
+		
+		public String VerifyProductDeActivationPrepaid() {
+			return KUSSD.VerifyProductDeActivationPrepaid();
+		}
+		
+		// ---------------------Mobile Usages Ends------------------------//
+		
+		
+		// ---------------------MCare Keywords Starts------------------------//
+		public String installMyVodafoneApp() {
+			return MCare.installMyVodafoneApp();
+		}
+		public String verifyMCareLogin() {
+			return MCare.verifyMCareLogin();
+		}
+		public String verifyPlanNameMCare() {
+			return MCare.verifyPlanNameMCare();
+		}
+		public String billEnquiryMCare() {
+			return MCare.billEnquiryMCare();
+		}
+		
+		// ---------------------MCare Keywords Ends------------------------//
+}
---
+++ Framework/Libraries/Keyword_DB.java
@@ -0,0 +1,217 @@
+package Libraries;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
+import java.util.ArrayList;
+
+import org.apache.commons.lang3.exception.ExceptionUtils;
+
+public class Keyword_DB extends Driver {
+
+	public static ThreadLocal<Connection> con = new ThreadLocal<Connection>();
+
+	public String DBConnection() {
+		String Test_OutPut = "", Status = "";
+		Result.fUpdateLog("------DB connection Event Details------");
+		String ServiceName = "";
+
+		if (getdata("Application_Details").equals("BRM_DB")) {
+			ServiceName = "BRMPROD";
+		} else if (getdata("Application_Details").equals("CRM_DB")) {
+			ServiceName = "CRMPROD";
+		}
+		String Username = getdata("VQ_Login_User").trim();
+		String password = getdata("VQ_Login_Pswd").trim();
+		String Host = getdata("URL/HOST");
+		int Port = 1521;
+
+		String driver = "oracle.jdbc.OracleDriver";
+		try {
+			Class.forName(driver);
+			con.set(DriverManager.getConnection("jdbc:oracle:thin:@" + Host + ":" + Port + ":" + ServiceName, Username,
+					password));
+			Result.fUpdateLog("connected to DB");
+
+			Result.fUpdateLog("Connected to DB: " + Host);
+			Test_OutPut += "Connected to DB: " + Host + ",";
+			Status = "PASS";
+		} catch (Exception e) {
+			Continue.set(false);
+			Test_OutPut += "Unable to connect to DB: " + Host + ServiceName + ",";
+			Result.fUpdateLog("Unable to connect to DB *** " + e.getMessage());
+			Status = "FAIL";
+			e.printStackTrace();
+		}
+		Result.fUpdateLog("------DB connection Event Details - Completed------");
+		return Status + "@@" + Test_OutPut + "<br/>";
+	}
+
+	public String DBDisconnection() {
+		String Test_OutPut = "", Status = "";
+		Result.fUpdateLog("------DB Disconnection Event Details------");
+		try {
+			con.get().close();
+			Result.fUpdateLog("DB disconnected successfully");
+			Test_OutPut += "DB disconnected successfully" + ",";
+			Status = "PASS";
+		} catch (Exception e) {
+			Continue.set(false);
+			Test_OutPut += "Failed to disconnect DB" + ",";
+			Result.fUpdateLog("Exception occurred *** " + ExceptionUtils.getStackTrace(e));
+			Status = "FAIL";
+			e.printStackTrace();
+		}
+		Result.fUpdateLog("------DB Disconnection Event Details - Completed------");
+		return Status + "@@" + Test_OutPut + "<br/>";
+	}
+
+	public String BillPoID() {
+		String Test_OutPut = "", Status = "", AccountNo = "";
+		if (Continue.get()) {
+
+			String BillingProfileName = "";
+			Result.fUpdateLog("------BillPoID Event Details------");
+
+			AccountNo = getdata("AccountNo");
+
+			if (!(getdata("BillingProfile").equals(""))) {
+				BillingProfileName = getdata("BillingProfile");
+			} else {
+				BillingProfileName = Utlities.FetchStoredValue(UseCaseName.get(), TestCaseN.get(),
+						"BillingProfileName");
+			}
+			try {
+				Statement statement = con.get().createStatement();
+				String queryString = "Select a.account_no,b.poid_id0,b.bill_info_id from pin.account_t a,pin.billinfo_t b where a.account_no in '"
+						+ AccountNo + "' and b.account_obj_id0=a.poid_id0 and b.bill_info_id='" + BillingProfileName
+						+ "'";
+
+				ResultSet rs = statement.executeQuery(queryString);
+				while (rs.next()) {
+					Test_OutPut = "BillPoID: " + rs.getString(2) + " BillingProfileName: " + rs.getString(3) + ",";
+					Utlities.StoreValue("BillPoID", rs.getString(2));
+				}
+				Status = "PASS";
+
+			} catch (Exception e) {
+				Continue.set(false);
+				Test_OutPut += "Failed to BillPoID" + ",";
+				Result.fUpdateLog("Exception occurred *** " + ExceptionUtils.getStackTrace(e));
+				Status = "FAIL";
+				e.printStackTrace();
+			}
+			Result.fUpdateLog("------BillPoID Event Details - Completed------");
+		} else {
+
+			Status = "FAIL";
+		}
+		return Status + "@@" + Test_OutPut + "<br/>";
+
+	}
+
+	public String AccPoID_BillPoID(String AccountNo) {
+		Continue.set(true);
+		String Test_OutPut = "";
+		if (Continue.get()) {
+			Result.fUpdateLog("------AccPoID_BillPoID Event Details------");
+			Test_OutPut = "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\r\n" + "<BusinessConfiguration\r\n"
+					+ "xmlns=\"http://www.portal.com/schemas/BusinessConfig\"\r\n"
+					+ "xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\"\r\n"
+					+ "xsi:schemaLocation=\"http://www.portal.com/schemas/BusinessConfig BusinessConfiguration.xsd\">\r\n"
+					+ "<!-- Sample input file for pin_bill_accts containing parameters for bill run management -->\r\n"
+					+ "<!-- Modify according to guidelines -->\r\n" + "<BillRunConfiguration>\r\n"
+					+ "<!-- List of Billinfo to be billed -->\r\n";
+			try {
+				Statement statement = con.get().createStatement();
+				String queryString = "Select a.poid_id0 ,b.poid_id0 from pin.account_t a,pin.billinfo_t b where a.account_no IN ('"
+						+ AccountNo + "') and b.account_obj_id0=a.poid_id0 AND NOT b.PAYINFO_OBJ_TYPE LIKE '%prepaid%'";
+				Result.fUpdateLog(queryString);
+				ResultSet rs = statement.executeQuery(queryString);
+				while (rs.next()) {
+					Test_OutPut += "        <BillingList>\r\n" + "                <Account>" + rs.getString(1)
+							+ "</Account>\r\n" + "                <Billinfo>" + rs.getString(2) + "</Billinfo>\r\n"
+							+ "        </BillingList>\r\n";
+				}
+
+				Test_OutPut += "</BillRunConfiguration>\r\n" + "</BusinessConfiguration>";
+			} catch (Exception e) {
+				Continue.set(false);
+				Test_OutPut += "Failed to AccPoID_BillPoID" + ",";
+				Result.fUpdateLog("Exception occurred *** " + ExceptionUtils.getStackTrace(e));
+				e.printStackTrace();
+			}
+
+		} else {
+			Result.fUpdateLog("Failed at intial did not entered in AccPoID_BillPoID");
+			Continue.set(false);
+		}
+		Result.fUpdateLog("------AccPoID_BillPoID Event Details - Completed------");
+		return Test_OutPut;
+	}
+
+	public String BillPoID(String AccountNo) {
+		Continue.set(true);
+		String Test_OutPut = "";
+		if (Continue.get()) {
+			Result.fUpdateLog("------BillPoID Event Details------");
+			Test_OutPut = "";
+			try {
+				Statement statement = con.get().createStatement();
+				String queryString = "select b.poid_id0 from pin.bill_t b , pin.account_t a, pin.billinfo_t bi where b.account_obj_id0 = a.poid_id0 and b.billinfo_obj_id0 = bi.poid_id0 and "
+						+ "bi.pay_type in ('10001','15003','15004') and b.invoice_obj_id0 = 0 and b.bill_no is not null and a.account_no in ('"
+						+ AccountNo + "')";
+				Result.fUpdateLog(queryString);
+				ResultSet rs = statement.executeQuery(queryString);
+				int i = 0;
+				while (rs.next()) {
+					Test_OutPut += "0 PIN_FLD_RESULTS     ARRAY [" + i + "]\r\n"
+							+ "1    PIN_FLD_POID        POID [0] 0.0.0.1 /bill " +  rs.getString(1) + "\r\n";
+					i++;
+				}
+			} catch (Exception e) {
+				Continue.set(false);
+				Test_OutPut += "Failed to BillPoID" + ",";
+				Result.fUpdateLog("Exception occurred *** " + ExceptionUtils.getStackTrace(e));
+				e.printStackTrace();
+			}
+
+		} else {
+			Result.fUpdateLog("Failed at intial did not entered in BillPoID");
+			Continue.set(false);
+		}
+		Result.fUpdateLog("------BillPoID Event Details - Completed------");
+		return Test_OutPut;
+
+	}
+	
+	public ArrayList<String> ACCPoID(String AccountNo) {
+		Continue.set(true);
+		ArrayList<String> arr = new ArrayList<String>();
+		if (Continue.get()) {
+			Result.fUpdateLog("------ACCPoID Event Details------");
+			try {
+				Statement statement = con.get().createStatement();
+				String queryString = "Select a.poid_id0 from pin.account_t a where a.account_no IN ('"
+						+ AccountNo + "')";
+				Result.fUpdateLog(queryString);
+				ResultSet rs = statement.executeQuery(queryString);
+				while (rs.next()) {
+					arr.add(rs.getString(1));
+				}
+			} catch (Exception e) {
+				Continue.set(false);
+				Result.fUpdateLog("Exception occurred *** " + ExceptionUtils.getStackTrace(e));
+				e.printStackTrace();
+			}
+
+		} else {
+			Result.fUpdateLog("Failed at intial did not entered in ACCPoID");
+			Continue.set(false);
+		}
+		Result.fUpdateLog("------ACCPoID Event Details - Completed------");
+		return arr;
+
+	}
+}
